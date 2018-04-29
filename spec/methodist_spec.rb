cbadd4a17942db8170059645bab790229320ea8f
Sergey Nesterov <qnesterr@gmail.com>
2018-04-29 19:10:51 +0300

    Builder

--- spec/methodist_spec.rb
+++ spec/methodist_spec.rb
@@ -4,4 +4,5 @@ RSpec.describe Methodist do
   it { expect(Methodist).to be_instance_of(Module) }
   it { expect(Methodist::Interactor).to be_instance_of(Class) }
   it { expect(Methodist::Observer).to be_instance_of(Class) }
+  it { expect(Methodist::Builder).to be_instance_of(Class) }
 end

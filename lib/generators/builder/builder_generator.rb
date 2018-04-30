278035aa83b5f3b4e338bc6cfe42e9cb4a05721f
Sergey Nesterov <qnesterr@gmail.com>
2018-04-30 15:59:41 +0300

    Builder generator

---
+++ lib/generators/builder/builder_generator.rb
@@ -0,0 +1,27 @@
+require_relative '../methodist_generator'
+
+class BuilderGenerator < MethodistGenerator
+  desc 'Create builder'
+  source_root File.expand_path('templates', __dir__)
+
+  PATTERN_FOLDER     = 'builders'.freeze
+  TEMPLATE_FILE      = 'builder.erb'.freeze
+  TEMPLATE_SPEC_FILE = 'builder_spec.erb'.freeze
+
+  class_option 'path',  type: :string,  desc: "Parent module for new builder", default: PATTERN_FOLDER
+
+  def generate
+    template(
+      TEMPLATE_FILE,
+      "#{filename_with_path}_builder.rb"
+    )
+  end
+
+  def generate_spec
+    template(
+      TEMPLATE_SPEC_FILE,
+      "#{filename_with_path(prefix: 'spec')}_builder_spec.rb"
+    )
+  end
+end
+

ed5670bfb5232d2150e2a31a393ab9372f4ed8a0
Sergey Nesterov <qnesterr@gmail.com>
2018-04-30 23:28:31 +0300

    Comments for builder and service

--- lib/methodist/service.rb
+++ lib/methodist/service.rb
@@ -4,11 +4,21 @@ class Methodist::Service < Methodist::Pattern
   CONST_CLIENT = 'CLIENT'
 
   class << self
+    ##
+    # Define client for service
+    #
+    # ==== Parameters
+    # * +client_instance+ [Instance of client klass] - Instance of client class.
+    # It could be different clients like redis, elastic, internal api etc.
+    ##
     def client(client_instance)
       const_set(CONST_CLIENT, client_instance)
     end
   end
 
+  ##
+  # Instance method use to get defined client
+  ##
   def client
     @client ||= self.class.const_get(CONST_CLIENT) #rescue nil
   end

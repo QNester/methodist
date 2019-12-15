1a27f5593fca92f3df7eb5f96369f0cc470fdc6a
Sergey Nesterov <qnesterr@gmail.com>
2019-12-15 21:52:02 +0300

    Option `parent` for generators

--- lib/generators/client/client_generator.rb
+++ lib/generators/client/client_generator.rb
@@ -7,8 +7,10 @@ class ClientGenerator < MethodistGenerator
   PATTERN_FOLDER     = 'clients'.freeze
   TEMPLATE_FILE      = 'client.erb'.freeze
   TEMPLATE_SPEC_FILE = 'client_spec.erb'.freeze
+  PARENT_CLASS       = 'Methodist::Client'.freeze
 
   class_option 'path',  type: :string,  desc: "Parent module for a new client", default: PATTERN_FOLDER
+  class_option 'parent', type: :string, desc: "Parent class of generated class", default: PARENT_CLASS
 
   def generate
     template(
--- lib/generators/interactor/interactor_generator.rb
+++ lib/generators/interactor/interactor_generator.rb
@@ -7,9 +7,11 @@ class InteractorGenerator < MethodistGenerator
   PATTERN_FOLDER     = 'interactors'.freeze
   TEMPLATE_FILE      = 'interactor.erb'.freeze
   TEMPLATE_SPEC_FILE = 'interactor_spec.erb'.freeze
+  PARENT_CLASS       = 'Methodist::Interactor'.freeze
 
   class_option 'skip-validations', type: :boolean, desc: "Skip validations in generated files", default: false
   class_option 'path',  type: :string,  desc: "Parent module for a new interactor", default: PATTERN_FOLDER
+  class_option 'parent', type: :string, desc: "Parent class of generated class", default: PARENT_CLASS
 
   def generate
     template(
